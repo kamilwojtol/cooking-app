ed9f2c2e4c6dcb2efb4a4acfcad7051f1ef75e45
Kamil Wojtol <kamil.wojtol97@gmail.com>
2021-10-04 15:48:33 +0200

    initial commit & added about page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).use(router).mount('#app')
+library.add(faLinkedin, faTwitter, faGithub);
+
+createApp(App)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,87 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="about-me">
+      <img class="image" src="../assets/me.jpg" alt="Photo of Kamil Wojtol" />
+      <h2 class="title">
+        This site was created by
+        <a class="title-link" href="https://github.com/kamilwojtol/"
+          >Kamil Wojtol</a
+        >
+      </h2>
+    </div>
+    <div class="about-social">
+      <h3 class="social-title">You can find me there:</h3>
+      <div class="social-icons">
+        <font-awesome-icon
+          :icon="['fab', 'linkedin']"
+          size="2x"
+          class="social-icon"
+        />
+        <font-awesome-icon
+          :icon="['fab', 'twitter']"
+          size="2x"
+          class="social-icon"
+        />
+        <font-awesome-icon
+          :icon="['fab', 'github']"
+          size="2x"
+          class="social-icon"
+        />
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.image {
+  height: 200px;
+  width: 200px;
+  border-radius: 50%;
+  border: 3px solid #94c973;
+}
+
+.title {
+  font-size: 28px;
+  line-height: 1.5;
+  margin-top: 25px;
+
+  .title-link {
+    color: #94c973;
+    text-decoration: none;
+    display: block;
+    transition: 0.7s all ease-in-out;
+
+    &:hover {
+      transition: 0.7s all ease-in-out;
+      font-size: 32px;
+    }
+  }
+}
+
+.about-social {
+  margin-top: 40px;
+
+  .social-title {
+    font-size: 20px;
+  }
+
+  .social-icons {
+    margin: 10px 0;
+
+    .social-icon {
+      margin: 0 10px;
+      transition: 0.7s all ease-in-out;
+
+      &:hover {
+        cursor: pointer;
+        color: #94c973;
+        transition: 0.7s all ease-in-out;
+      }
+    }
+  }
+}
+</style>
